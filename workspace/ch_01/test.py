8bd744da207b01b5d98ee7c7d06d4a792ce47309
athinking <athinking@easyredmine.co.kr>
2026-01-29 16:48:27 +0900

    class추가

---
+++ workspace/ch_01/test.py
@@ -0,0 +1,17 @@
+#msg = "Hello, world!"
+#print(msg)
+
+msg = "" 
+number = 0
+b_true = True
+fnum = 0.0
+
+list_data = []
+tuple_data = ()
+dict_data = {}
+set_data = set()
+
+
+print(msg, number, b_true, fnum)
+print(type(msg), type(number), type(b_true), type(fnum))
+print(type(list_data), type(tuple_data), type(dict_data), type(set_data))
